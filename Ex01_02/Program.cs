d1d3a6dab0ea7aa2befd3198e3221b81a76fac73
Liav Simantov <liavsi@mta.ac.il>
2023-03-27 11:22:43 +0300

    ex3 finishes and fix name of public methods

--- Ex01_02/Program.cs
+++ Ex01_02/Program.cs
@@ -10,35 +10,38 @@ namespace Ex01_02
     {
         static public void Main()
         {
-            printDiamond(5, 1);
+            PrintDiamond(5, 1);
         }
 
-        static public void printDiamond(int i_LongestRow, int i_CurrentRow)
+        static public void PrintDiamond(int i_LongestRow, int i_CurrentRow)
         {
             if (i_LongestRow <= i_CurrentRow)
             {
-                printRowOfDiamond(i_LongestRow, i_CurrentRow);
+                PrintRowOfDiamond(i_LongestRow, i_CurrentRow);
             }
             else
             {
-                printRowOfDiamond(i_LongestRow, i_CurrentRow);
-                printDiamond(i_LongestRow, i_CurrentRow + 1);
-                printRowOfDiamond(i_LongestRow, i_CurrentRow);
+                PrintRowOfDiamond(i_LongestRow, i_CurrentRow);
+                PrintDiamond(i_LongestRow, i_CurrentRow + 1);
+                PrintRowOfDiamond(i_LongestRow, i_CurrentRow);
             }
         }
 
-        static public void printRowOfDiamond(int i_LongestRow, int i_RowToPrint)
+        static public void PrintRowOfDiamond(int i_LongestRow, int i_RowToPrint)
         {
             int numberOfSpaces = i_LongestRow - i_RowToPrint;
             int numberOfStars = i_RowToPrint * 2 - 1;
+
             for (int i = 0; i < numberOfSpaces; i++)
             {
                 Console.Write(" ");
             }
+
             for (int i = 0; i < numberOfStars; i++)
             {
                 Console.Write("*");
             }
+
             Console.WriteLine();
         }
     }
--- Ex01_03/Program.cs
+++ Ex01_03/Program.cs
@@ -6,18 +6,33 @@ using System.Threading.Tasks;
 
 namespace Ex01_03
 {
-   // class Program
-   // {
-   //     public static void Main()
-   //     {
-   //         int heightOfDiamond = usersInputHeight();
-   //         Ex01_02.Program.printDiamond()
-   //     }
-   //
-   //     public static int usersInputHeight()
-   //     {
-   //         Console.WriteLine("Please enter a number of ")
-   //         int inputHeight = int.Parse(Console.ReadLine());
-   //     }
-   // }
+    class Program
+    {
+        public static void Main()
+        {
+            while(true)
+            {
+                int heightOfDiamond = UsersInputHeight();
+                Ex01_02.Program.PrintDiamond(heightOfDiamond, 1);
+            }
+        }
+   
+        public static int UsersInputHeight()
+        {
+            int defaultHeight = 9;
+            string invalidInputMessage = string.Format("height must be a positive value!\ndefault value {0} will be printed", defaultHeight);
+
+            Console.WriteLine("Please enter the height of the diamond:(then press enter) ");
+            int inputHeight = int.Parse(Console.ReadLine());
+            if (inputHeight < 0)
+            {
+                Console.WriteLine(invalidInputMessage);
+                inputHeight = defaultHeight;
+            }
+
+            int retVal = (inputHeight / 2) + 1;
+
+            return retVal;
+        }
+    }
 }
