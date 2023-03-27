9cebe916c99f272ae72dbeeab52cbba873be1aeb
Liav Simantov <liavsi@mta.ac.il>
2023-03-27 12:45:54 +0300

    add project 5

---
+++ Ex01_05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_05
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string sixDigitNumber = GetSixDigitNumberFromUser();
+
+        }
+    }
+}
